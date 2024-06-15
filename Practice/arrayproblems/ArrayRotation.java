c39556a27b8ede3425c39ae147c038c6cb216af8
logesh028 <logesht2003@gmail.com>
2024-06-15 14:52:49 +0530

    Array prctise

--- Practice/arrayproblems/ArrayRotation.java
+++ Practice/arrayproblems/ArrayRotation.java
@@ -1,6 +1,16 @@
 package Practice.arrayproblems;
 import java.util.Scanner;
 public class ArrayRotation {
+
+    public static int[] arrayReverse(int arr[], int size) {
+        int[] reversedArray = new int[size];
+        for (int i = size - 1, j = 0; i >= 0; i--, j++) {
+            reversedArray[j] = arr[i];
+        }
+        return reversedArray;
+    }
+    
+    
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int size=  s.nextInt();
@@ -8,15 +18,18 @@ public class ArrayRotation {
         for(int i=0;i<size;i++){
             arr[i]=s.nextInt();
         }
-        int k = s.nextInt();
+        //nt k = s.nextInt();
 
-        // for(int i=0;i<size;i++){
-        //     System.out.print(arr[i]+" ");
-        // }
-        
-        for(int i=size-1;i>=0;i--){
-                System.out.print(arr[i]+" ");
+        int z[] = arrayReverse(arr, size);
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(z[i] + " ");
         }
+        
+        // for(int i=size-1;i>=0;i--){
+        //         System.out.print(arr[i]+" ");
+        // }
+
 
         // for(int i: arr){
         //     System.out.print(i+" ");
--- Practice/arrayproblems/RemoveDuplicate.java
+++ Practice/arrayproblems/RemoveDuplicate.java
@@ -1,12 +1,37 @@
 package Practice.arrayproblems;
 
 import java.util.Scanner;
-
+    
 public class RemoveDuplicate {
+
+    public static void removeduplicate(int[] a, int size) {
+        int temp[] = new int[size];
+        int j=0;
+        for(int i=0;i<size-1;i++){
+            if(a[i]!=a[i+1]){
+                temp[j++] = a[i];
+            }
+        }
+        temp[j++] = a[size-1];
+        // for(int num:temp){
+        //     System.out.print(num+" ");
+        // }
+        
+        for(int i=0;i<j;i++){
+        System.out.print(temp[i]+" ");
+    }
+    } 
+    
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int size =s.nextInt();
         int arr[]=new int[size];
-         
+
+        for(int i=0;i<size;i++){
+            arr[i]=s.nextInt();
+        }
+         removeduplicate(arr,size);
     }
+
+    
 }
